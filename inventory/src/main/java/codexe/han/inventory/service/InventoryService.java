bdebf224639a32ddf92e05471673671eedc546fc
Zhenchao Bing <bingzhenchao@mozat.com>
2019-07-16 18:42:36 +0800

    cache aside pattern read miss

--- inventory/src/main/java/codexe/han/inventory/service/InventoryService.java
+++ inventory/src/main/java/codexe/han/inventory/service/InventoryService.java
@@ -3,9 +3,11 @@ package codexe.han.inventory.service;
 public interface InventoryService {
     /**
      * 日常业务，读多写少，我们可以先更新数据库库存，然后invalidate 缓存
-     *  可能出现的不一致情况：
+     *  可能出现的不一致情况：缓存刚好失效，A读取数据，然后B更新数据，invaldiate缓存，A读到的旧数据刚到，写入
+     *  解决方案：是否可以存储一个最后更新的时间？按照数据库的时间戳？
      * 写多读少的情况下，先invalidate 缓存，在更新数据库
-     *  可能出现的不一会情况：
+     *  可能出现的不一会情况：更新没有完成，就有读请求，会读到脏数据
+     *  解决方案：串行化读写请求
      * cache aside pattern 是先更新数据库 再删除缓存 facebook也是
      */
     boolean blockInventoryWeakConsistent(long inventoryId, int amount);
